bb709ab78dd9c8ee30ac86c5a721cfcae4625994
Rishikesh Jadhav <rishijadhav2792@gmail.com>
2022-01-17 22:50:32 +0530

    Material Components Test

--- scope/ui/my-button/src/my-button.component.ts
+++ scope/ui/my-button/src/my-button.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'my-button',
-  template: `
-    <p>
-    my-button works!
-    </p>
-`,
+  templateUrl: './my-button.component.html',
   styleUrls: ['./my-button.component.scss']
 })
 export class MyButtonComponent {
-  constructor() {}
+
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl(),
+  });
+
+  constructor() {
+    this.filteredOptions = new Observable<string[]>();
+  }
+
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value)),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 }
--- scope/ui/my-button/src/my-button.module.ts
+++ scope/ui/my-button/src/my-button.module.ts
@@ -1,13 +1,47 @@
 import { NgModule } from '@angular/core';
 import { MyButtonComponent } from './my-button.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCommonModule, MatNativeDateModule } from '@angular/material/core';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+
+import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @NgModule({
   declarations: [
     MyButtonComponent
   ],
-  imports: [],
+  imports: [
+    MatCommonModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatMomentDateModule,
+    MatAutocompleteModule,
+    OverlayModule
+  ],
   exports: [
     MyButtonComponent
+  ],
+  providers: [
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
   ]
 })
-export class MyButtonModule {}
+export class MyButtonModule { }
